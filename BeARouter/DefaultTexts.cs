9e0361c1fae989157b014fd99210bea9fa615ac5
Philipp Albrecht <philipp@uisa.ch>
2021-12-17 03:05:57 +0100

    fixed window size and text

--- BeARouter/DefaultTexts.cs
+++ BeARouter/DefaultTexts.cs
@@ -9,7 +9,7 @@ namespace BeARouter
 
         public const string PRESS_START_TO_RECEIVE_FIRST_IPV4 = "Press start to receive first IPv4 packet.";
 
-        public const string IPv4PACKET_DESCRIPTION = "{0} is sending a IPv4 packet to {1}.\nSelect the interfaces to which the router should forward the IPv4 packet.";
+        public const string IPv4PACKET_DESCRIPTION = "{0} is sending an IPv4 packet to {1}.\nSelect the interfaces to which the router should forward the IPv4 packet.";
 
         public const string IPv4PACKET_RESULT = "{0}! Route {1} would have been selected. See explanation for more details.";
 
@@ -19,7 +19,7 @@ namespace BeARouter
 
         public const string EXPLAIN_DEST_IP = "The IPv4 packet has the destination {0}";
 
-        public const string ROUTE_EXPLAIN_LINE = "route {0,-19} has Suffix (CIDR) {1,-2} applied to dest. address results into net address {2,-19} => {3}";
+        public const string ROUTE_EXPLAIN_LINE = "route {0,-19} has Suffix (CIDR) {1,-2} -> applied to dest. address -> results into net address {2,-19} => {3}";
 
         public const string NO_MATCH = "NO MATCH";
 
