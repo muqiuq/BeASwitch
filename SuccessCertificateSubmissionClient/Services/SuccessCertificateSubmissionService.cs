cb567330d67fcbaa132116770e39d0a98aa3d165
Philipp Albrecht <philipp@uisa.ch>
2023-01-04 13:22:28 +0100

    Updated Framework to .net 6

---
+++ SuccessCertificateSubmissionClient/Services/SuccessCertificateSubmissionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuccessCertificateSubmissionClient.Services
+{
+    public class SuccessCertificateSubmissionService
+    {
+        public string
+    }
+}
